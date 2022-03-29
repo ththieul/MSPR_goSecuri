7106ea050b5a9f1c43db72c40fec247b18cc04a1
Naylink-x <58996827+Naylink-x@users.noreply.github.com>
2022-03-29 09:42:55 +0200

    corrected android app

--- GoSecuri/app/src/main/java/fr/epsi/gosecuri/BaseActivity.kt
+++ GoSecuri/app/src/main/java/fr/epsi/gosecuri/BaseActivity.kt
@@ -7,13 +7,6 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 open class BaseActivity: AppCompatActivity(){
-    fun showBack(){
-        val imageViewBack = findViewById<ImageView>(R.id.imageViewBack)
-        imageViewBack.visibility= View.VISIBLE
-        imageViewBack.setOnClickListener(View.OnClickListener {
-            onBackPressed()
-        })
-    }
 
     fun setHeaderTitle(text:String){
         val textViewTitle = findViewById<TextView>(R.id.textViewTitle)
